53efa2ca33921df9d319ede9f00f0fbf29cf5658
at1208 <amantiwari12081999@gmail.com>
2021-05-16 00:15:57 +0530

    blog style improved

--- components/Blog/Cards/largeCardSkeleton.js
+++ components/Blog/Cards/largeCardSkeleton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Skeleton } from 'antd';
 
 const LargeCardSkeleton = () => {
-  return <div className="row pl-4 pr-4">
+  return <div className="p-4">
           <Skeleton.Button active={true} size={300} shape="rectangle"   />
           <br />
           <Skeleton active paragraph={{ rows: 3 }} />
